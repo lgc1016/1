00528da3a6be3537ff5e5fbf7c5cf3210600f791
lgc1016 <135810520+lgc1016@users.noreply.github.com>
2023-06-23 00:44:42 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+function 모션3 () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+    basic.pause(7000)
+}
 function 모션1 () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     basic.pause(3000)
@@ -26,9 +31,41 @@ function 모션1 () {
     maqueen.motorStop(maqueen.Motors.All)
 }
 radio.onReceivedString(function (receivedString) {
+    while (receivedString == "C") {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+    while (receivedString == "E") {
+        music.play(music.stringPlayable("C5 G B A F A C5 B ", 120), music.PlaybackMode.UntilDone)
+    }
     if (receivedString == "A") {
         모션1()
         radio.sendString("B")
+    } else if (receivedString == "C") {
+        모션2()
+        radio.sendString("D")
+    } else if (receivedString == "E") {
+        모션3()
+    } else {
+    	
     }
 })
+function 모션2 () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    for (let index = 0; index < 3; index++) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        basic.pause(200)
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.pause(200)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
+    basic.pause(1000)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+}
 radio.setGroup(5)
+basic.showNumber(1)
